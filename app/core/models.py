9d3618ba13f7ebc1e9bafa4e30e444dc629c3f28
hainguyen <hainguyen4391@gmail.com>
2021-07-15 14:17:48 +0700

    emplement recipe app

--- app/core/models.py
+++ app/core/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
                                         PermissionsMixin
+from django.conf import settings
+
 
 # Create your models here.
 class UserManager(BaseUserManager):
@@ -35,3 +37,12 @@ class User(AbstractBaseUser, PermissionsMixin):
   USERNAME_FIELD = 'email'
 
   
+class Tag(models.Model):
+  name = models.CharField(max_length=255)
+  user = models.ForeignKey(
+    settings.AUTH_USER_MODEL,
+    on_delete=models.CASCADE,
+  )
+
+  def __str__(self):
+    return self.name
